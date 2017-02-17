cc32b7d5d43d1a3ae77c36d1a744a644c0fb3f4a
similar718 <1170214556@qq.com>
2017-02-17 11:08:39 +0800

    html ok

--- CameraAndGPS/app/src/main/java/com/camera/operation/cameraandgps/ui/MessageActivity.java
+++ CameraAndGPS/app/src/main/java/com/camera/operation/cameraandgps/ui/MessageActivity.java
@@ -1,34 +1,47 @@
 package com.camera.operation.cameraandgps.ui;
 
+import android.annotation.TargetApi;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.camera.operation.cameraandgps.R;
 import com.camera.operation.cameraandgps.util.Constants;
 import com.camera.operation.cameraandgps.view.TopbarView;
 
 /**
- * Created by similar on 2017/2/10.
+ * Created by similar on 2017/2/17.
  */
 
 public class MessageActivity extends AppCompatActivity {
-    private WebView mWebView;
+
+    private WebView wv;
     private TopbarView mTopBar;
     private ImageView mLeft;
+    //private String url ="http://shanghai.job1s.com/wap/member/index.php?c=photo";
+
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
-        initView();
-    }
+        setContentView(R.layout.activity_control);
 
-    private void initView(){
-        mWebView = (WebView) findViewById(R.id.message_wv);
-        mTopBar = (TopbarView) findViewById(R.id.message_tv);
+        mTopBar = (TopbarView) findViewById(R.id.control_tv);
         mTopBar.setMiddleText(getResources().getString(R.string.Main_Message));
         mLeft = (ImageView) mTopBar.findViewById(R.id.left_btn);
         mLeft.setImageResource(R.drawable.photo_back_selector);
@@ -40,6 +53,163 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
-        mWebView.loadUrl(Constants.mMessageUrl);
+        wv = (WebView) findViewById(R.id.control_wv);
+        //设置支持Javascript
+        wv.getSettings().setJavaScriptEnabled(true);
+
+        wv.loadUrl(Constants.mControlUrl);
+        wv.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+                view.loadUrl(url);
+                return false;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+            }
+        });
+
+
+        wv.setWebChromeClient(new WebChromeClient(){
+
+            // For 3.0+ Devices (Start)
+            // onActivityResult attached before constructor
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+            {
+                mUploadMessage = uploadMsg;
+//                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+//                i.addCategory(Intent.CATEGORY_OPENABLE);
+//                i.setType("image/*");
+
+                //直接选择不用确认
+                Intent intent = new Intent();
+				 /* 开启Pictures画面Type设定为image */
+                intent.setType("image/*");
+				 /* 使用Intent.ACTION_GET_CONTENT这个Action */
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+//                //需要确认之后确定图片
+//                Intent intent;
+//                if (Build.VERSION.SDK_INT < 19) {
+//                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                    intent.setType("image/*");
+//                } else {
+//                    intent = new Intent(
+//                            Intent.ACTION_PICK,
+//                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                }
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try
+                {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e)
+                {
+                    uploadMessage = null;
+                    Toast.makeText(getBaseContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+            {
+                mUploadMessage = uploadMsg;
+//                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                intent.addCategory(Intent.CATEGORY_OPENABLE);
+//                intent.setType("image/*");
+
+                //直接选择不用确认
+                Intent intent = new Intent();
+				 /* 开启Pictures画面Type设定为image */
+                intent.setType("image/*");
+				 /* 使用Intent.ACTION_GET_CONTENT这个Action */
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+//                //需要确认之后确定图片
+//                Intent intent;
+//                if (Build.VERSION.SDK_INT < 19) {
+//                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                    intent.setType("image/*");
+//                } else {
+//                    intent = new Intent(
+//                            Intent.ACTION_PICK,
+//                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                }
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+            {
+                mUploadMessage = uploadMsg;
+//                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+//                i.addCategory(Intent.CATEGORY_OPENABLE);
+//                i.setType("image/*");
+
+                //直接选择不用确认
+                Intent intent = new Intent();
+				 /* 开启Pictures画面Type设定为image */
+                intent.setType("image/*");
+				 /* 使用Intent.ACTION_GET_CONTENT这个Action */
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+//                //需要确认之后确定图片
+//                Intent intent;
+//                if (Build.VERSION.SDK_INT < 19) {
+//                    intent = new Intent(Intent.ACTION_GET_CONTENT);
+//                    intent.setType("image/*");
+//                } else {
+//                    intent = new Intent(
+//                            Intent.ACTION_PICK,
+//                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//                }
+                startActivityForResult(Intent.createChooser(intent, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (requestCode == REQUEST_SELECT_FILE)
+            {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        }
+        else if (requestCode == FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage)
+                return;
+            Uri result = intent == null || resultCode != MessageActivity.RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+        else
+            Toast.makeText(getBaseContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
     }
 }
